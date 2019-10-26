b81461d368b7b8b6023607d3b05ad872e9a269a5
Victor <victorhugo_rosa@hotmail.com>
2019-10-26 09:50:39 -0300

    conversor de dolar

---
+++ ConversorDeMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorDeMoeda
+{
+    class ConversorDeMoeda
+    {   // Porcentagem de IOF
+        public static double Iof = 6.0/100.0;
+        // Método "VlrTotal" lembrando que as variaveis vlrD e qtdD 
+        //serão incrementadas na chamada do método na classe principal.
+        public static double VlrTotal(double vlrD,double qtdD)
+        {
+            double resultado = vlrD * qtdD;
+            return resultado + resultado * Iof;
+        }
+    }
+}
---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Entra com os dois valores para vlrDolar e qtdDolar
+            Console.Write("Qual o valor atual do dólar: ");
+            double vlrDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você pretende comprar: ");
+            double qtdDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            // Variavel vlrPago recebe a classe ConversorDeMoeda mais seu método VlrTotal
+            //usando os valores recebidos de entrada vlrDolar,qtdDolar.
+            double vlrPago = ConversorDeMoeda.VlrTotal(vlrDolar,qtdDolar);
+
+            Console.Write("O valor que você terá de pagar é: "+ vlrPago.ToString("f2",CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
